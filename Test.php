d19b1a66683194dd1b58ac73d8a30a45a4d88acd
bruce ding <dingjingdjdj@gmail.com>
2013-11-16 15:20:56 +0800

    测试type操作正常返回数据

--- Test.php
+++ Test.php
@@ -5,6 +5,13 @@ $fields = array();
 
 $fields[] = array('field' => 'name', 'value'=> array(1), 'type'=> 'string');
 
+// 测试type 操作 默认返回json
 $res = FormCheck::check($fields);
+print_r($res);
 
+// 测试正常返回
+$fields = array();
+$fields[] = array('field' => 'name', 'value'=> array(1), 'type'=> 'array');
+
+$res = FormCheck::check($fields);
 print_r($res);
